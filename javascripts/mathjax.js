ac81106121d23f871b90d584959fdf1ddb30729a
H0308 <1848312235@qq.com>
2024-08-09 09:14:55 +0800

    Deployed 09df335 with MkDocs version: 1.6.0

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,16 +1,20 @@
-window.MathJax = {
+MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
     processEscapes: true,
     processEnvironments: true
   },
   options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
   }
 };
 
-document$.subscribe(() => { 
-  MathJax.typesetPromise()
-})
+// If you want to delay the loading of MathJax until the page is ready
+document.addEventListener("DOMContentLoaded", function() {
+  window.MathJax = MathJax;
+  // Initialize MathJax
+  const script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+  document.head.appendChild(script);
+});

6893250e56e5bb87d120d8791cd0e13d0577b16b
EPSDA <74664113+H0308@users.noreply.github.com>
2024-12-07 03:21:29 +0000

    文档

--- helper_site/docs/javascripts/other.js
+++ helper_site/docs/javascripts/other.js
@@ -87,3 +87,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', updateScrollIndicator);
     updateScrollIndicator();
 });
+
+document.getElementsByClassName("md-top")[0].innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13 20h-2V8l-5.5 5.5-1.42-1.42L12 4.16l7.92 7.92-1.42 1.42L13 8z"></path></svg>';
